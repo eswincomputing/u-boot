c0e7d56ed695259898d921999fbd4ae5902a566f
huangyifeng <huangyifeng@eswincomputing.com>
2025-04-17 17:37:49 +0800

    fix:Support for D2D PMIX Data Auto-Update
    Changelogs:
        don't try to init die1 lpcou when single die
    
    Signed-off-by: huangyifeng <huangyifeng@eswincomputing.com>

--- drivers/mailbox/eswin-umbox-srvc.c
+++ drivers/mailbox/eswin-umbox-srvc.c
@@ -63,11 +63,13 @@ int lpcpu_misc_func(void)
 {
 	struct udevice *dev;
 	struct udevice *dev1;
+
 	// d2d pmix
-	uclass_get_device_by_name(UCLASS_MISC, "mbox_srvc1@d0", &dev);
-	eswin_umbox_service_get(dev);
-	uclass_get_device_by_name(UCLASS_MISC, "mbox_srvc1@d1", &dev1);
-	eswin_umbox_service_get(dev1);
+	if (!uclass_get_device_by_name(UCLASS_MISC, "mbox_srvc1@d0", &dev))
+		eswin_umbox_service_get(dev);
+
+	if(!uclass_get_device_by_name(UCLASS_MISC, "mbox_srvc1@d1", &dev1))
+		eswin_umbox_service_get(dev1);
 
 	return 0;
 }
