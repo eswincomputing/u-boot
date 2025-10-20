9e776c3b783b4fb65428dcedb4f2294b669d05fc
zhaodonghui <zhaodonghui@eswincomputing.com>
2025-10-20 21:20:29 +0800

    feat:Create U2 progress: config in U-Boot
    Changelogs:
    1. Create board, defconfig, dts in U-Boot.
    
    Signed-off-by: zhaodonghui <zhaodonghui@eswincomputing.com>
    Reviewed-by: Joey Zheng <zhengyu@eswincomputing.com>

---
+++ board/eswin/ebc7702_u01_pcie/ebc7702_u01_pcie.c
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * u2 evb board init
+ *
+ * Copyright 2024, Beijing ESWIN Computing Technology Co., Ltd.. All rights reserved.
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Authors: Joey Zheng <zhengyu@eswincomputing.com>
+ */
+#include <common.h>
+#include <dm.h>
+#include <linux/delay.h>
+#include <linux/io.h>
+#include "env.h"
+#include <errno.h>
+#include <blk.h>
+#include <net.h>
+#include <stdlib.h>
+#include <init.h>
+#include <spi.h>
+#include <spi_flash.h>
+#include <dm/uclass-internal.h>
+#include <dm/device-internal.h>
+#include <dm/pinctrl.h>
+#include <u-boot/crc.h>
+#include <eic7700_common.h>
+#include <eswin/cpu.h>
+#include <asm/gpio.h>
+#include <fdt_support.h>
+#ifdef CONFIG_ESWIN_UMBOX
+#include <eswin/eswin-umbox-srvc.h>
+#endif
+
+int misc_init_r(void)
+{
+
+#ifdef CONFIG_ESWIN_PMP
+	eswin_pmp_init();
+#endif
+
+#if defined(CONFIG_ESWIN_SPI)
+	es_bootspi_write_protection_init();
+#endif
+
+	if (NULL == env_get("fdtfile")) {
+		env_set("fdtfile","eswin/ebc7702-u01-pcie.dtb");
+	}
+	eswin_update_bootargs();
+	return 0;
+}
+
+
+
+void irq_mux_route(void)
+{
+	unsigned int val;
+
+	/* Route all interrupts from default LPCPU/SCPU to MCPU
+	 * I2C0 bit16
+	 * I2C1 bit15
+	 * RTC  bit14~13
+	 * SPI  bit11~10
+	 * DMA  bit9
+	 * MPMP bit8
+	 * TIMER1 bit5~4
+	 * TIMER2 bit3~2
+	 * TIMER3 bit1~0
+	 *
+	 * Route GPIO, TIMER0 to LPCPU
+	*/
+
+	val = 0x19040;
+	writel(val,(void *)(0x51810000+0x3c0));
+	writel(val,(void *)(0x71810000+0x3c0));
+}
+
+int board_init(void)
+{
+	irq_mux_route();
+	return 0;
+}
+
+int board_late_init(void)
+{
+#ifdef CONFIG_ESWIN_UMBOX
+//	lpcpu_misc_func();
+#endif
+	return 0;
+}
+
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	return fdt_fixup_memory(blob, gd->ram_base, gd->ram_size);
+}
